7a4e2407864d9c4cf18b6d0c0488ebab6b1ddaa5
GitHub Action <action@github.com>
2024-08-12 05:54:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/danthk/myawesomeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/danthk/myawesomeplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.danthk.myawesomeplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.danthk.myawesomeplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
